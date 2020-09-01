5d87696ce66309361e8ed17ef8f92f7bd3240b96
xvhaorong <hxx5086@psu.edu>
2020-09-01 21:53:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+number = input ("Enter temperature: ")
+type = input("Enter unit in F/f or C/c: ")
+
+if(type == "c"):
+  F= (float (number)*1.8)+32;
+  print(str(float(number))+ "° in Celsius is equivalent to " + str(F) + "° Fahrenheit.")
+
+elif(type== "f"):
+  C = (float (number)-32)*5/9;
+  print(str(float(number))+ "° in Fahrenheit is equivalent to " + str(C) + "Celsius° .")
+
+else:
+  print ("Invalid nuit ("+type+").")
+
