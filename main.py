f977d32a7d8699a7e48defbf060afd56ad0988c6
xvhaorong <hxx5086@psu.edu>
2020-09-01 22:47:54 +0000

    .

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 number = input ("Enter temperature: ")
 type = input("Enter unit in F/f or C/c: ")
 
-if(type == "c" or "C"):
+if(type == "c" == "C"):
   F= (float (number)*1.8)+32;
   print(f"{number}° in Celsius is equivalent to {F}° Fahrenheit.")
 
 
-elif(type== "f" or "F"):
+elif(type== "f" == "F"):
   C = (float (number)-32)*5/9;
   print(f"{number}° in Fahrenheit is equivalent to {C}° Celsius.")
 
