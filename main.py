7377577f7067b0054f740b4a2bd23e486b800724
xvhaorong <hxx5086@psu.edu>
2020-09-03 20:57:50 +0000

    final

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# Author: Haorong Xu hxx5086@psu.edu
+# Collaborator: Jerry Diaz jqd5879@psu.edu
+# Collaborator: Jocelyn Fafard jqf5530@psu.edu
+# Collaborator: Nicholas Cole nyc5096@psu.edu
+
+
 number = input ("Enter temperature: ")
 type = input("Enter unit in F/f or C/c: ")
 
-if type == "c" or type == "C":
+if type == "C" or type == "c":
   F= (float (number)*1.8)+32;
   print(f"{number}° in Celsius is equivalent to {F}° Fahrenheit.")
 
